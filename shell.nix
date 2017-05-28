4904a234034a52df54cd5b477c252c8d38a0a46e
Benjamin Saunders <ben.e.saunders@gmail.com>
2017-05-28 11:31:09 -0700

    Update nix env to mozilla overlay

--- shell.nix
+++ shell.nix
@@ -2,5 +2,5 @@ with import <nixpkgs> { };
 
 stdenv.mkDerivation {
   name = "wair";
-  buildInputs = (with pkgs; [ (rustNightly.rust { }) libevdev systemd libxkbcommon python3 pkgconfig xlibs.libX11 xlibs.libXi xlibs.libXext ]);
+  buildInputs = (with pkgs; [ rustChannels.stable.rust libevdev systemd libxkbcommon python3 pkgconfig xlibs.libX11 xlibs.libXi xlibs.libXext ]);
 }
